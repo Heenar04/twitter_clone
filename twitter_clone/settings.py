46f2851c119faf39ab09ff46ea703de50fb2c9e7
hrajpal <hrajpal@ebrevia.com>
2021-06-17 22:27:36 -0400

    added debug true

--- twitter_clone/settings.py
+++ twitter_clone/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h)-7kl0_$d76jk5#4ut01$l+xg%6)6b&81_m6()vf2ft8br_-d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
